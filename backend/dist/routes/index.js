07e794b4dda1276263f7f148c53cd9b0f2730a38
SiwarAbid <abidsiwar371@gmail.com>
2024-02-05 11:08:59 +0100

    commits

---
+++ backend/dist/routes/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.routerUser = void 0;
+const express_1 = __importDefault(require("express"));
+exports.routerUser = express_1.default.Router();
